c040c6a53203d5f24a5230e4c5ae2341c06c08a8
antsimch <anthonysimch@gmail.com>
2023-08-22 11:10:40 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/csf35_workshop/Csf35WorkshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.csf35_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csf35WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csf35WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/csf35_workshop/Csf35WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.csf35_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csf35WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
